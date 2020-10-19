aa2e9ef8e2154f0884dc87536f5be2c3a96d01fb
José Cage <jose.cage@doesangue.me>
2020-10-19 03:28:08 +0100

    Fixed audio transformation service

--- app/Services/SpeechService.php
+++ app/Services/SpeechService.php
@@ -3,10 +3,10 @@
 namespace App\Services;
 
 use Illuminate\Support\Str;
-use AWS;
 
 class SpeechService
 {
+
     /**
      * @param $input
      * @param array $options
@@ -14,7 +14,7 @@ class SpeechService
      */
     public static function transformToVoice($input, $options = [])
     {
-        $client = AWS::createClient('polly');
+        $client = \AWS::createClient('polly');
 
         try {
             $config = [
@@ -24,19 +24,37 @@ class SpeechService
                 'VoiceId' => $options['voice_id'] ?: 'Emma'
             ];
 
-            $synthesizeText = $client->syntesizeSpeech($config);
+            $synthesizeText = $client->synthesizeSpeech($config);
 
-            $audioFileName =Str::uuid();
+            $audioFileName = Str::uuid();
 
             // Save the file to the storage or User specific provider
-            file_put_contents(storage_path('audio'), $audioFileName . 'mp3', $synthesizeText['AudioStream']);
+            file_put_contents(storage_path('audio/'. $audioFileName . '.mp3'), $synthesizeText['AudioStream']);
 
-            echo 'Converted audio saved as ' . $audioFileName . '"mp3"' . PHP_EOL;
+            echo 'Converted audio saved as ' . $audioFileName . '.mp3' . PHP_EOL;
 
             return $synthesizeText;
 
         } catch (PollyException $exception) {
             throw $exception;
         }
+    }
+
+    /**
+     * @param string $engine
+     * @param string|null $code
+     * @return mixed
+     */
+    public static function listAvailableVoices(string $engine, string $code = null)
+    {
+        $client = AWS::createClient('polly');
+
+        $voices = $client->describeVoices([
+            'Engine' => $engine ?: 'standard|neural',
+            'IncludeAdditionalLanguageCodes' => true | false,
+            'LanguageCode' => $code ?: 'en-US|en-ES|en-DE|pt-PT',
+        ]);
+
+        return $voices;
     }
 }
